d4cf2a7fa57be90f7c888bf9051a75469652be84
Hollman Rojas <hollmanerojas@gmail.com>
2024-12-17 16:10:56 -0500

    Feature/config-viws-for-apirest

--- tourism_admin/admin_panel/views.py
+++ tourism_admin/admin_panel/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from tourism_admin.admin_panel.models import Merchant
+from tourism_admin.admin_panel.serializers import MerchantSerializer
+
+
+class MerchantListView(APIView):
+    def get(self, request):
+        merchants = Merchant.objects.all()
+        serializer = MerchantSerializer(merchants, many=True)
+        return Response(serializer.data)
