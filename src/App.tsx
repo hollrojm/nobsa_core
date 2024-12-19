4feb7fc600b040e6ea55006c75f5010df4401cdf
Hollman Rojas <hollmanerojas@gmail.com>
2024-12-19 02:05:45 -0500

    Feature/ config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { useState } from "react";
-import { MerchantPage } from "./pages/MerchantsPage";
+//import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { MerchantPage } from "./pages/MerchantsPage";
 import { MerchantFormPage } from "./pages/MerchantsFormPage";
 
-function App() {
-  const [] = useState(0);
+const App: React.FC = () => {
+  // const [count, setCount] = useState<number>(0);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/merchants" />}></Route>
-        <Route path="/merchants" element={<MerchantPage />}></Route>
-        <Route path="/merchants-create" element={<MerchantFormPage />}></Route>
+        {/* Redirige automáticamente a la página de merchants */}
+        <Route path="/" element={<Navigate to="/merchants" />} />
+
+        {/* Página principal de merchants */}
+        <Route path="/merchants" element={<MerchantPage />} />
+
+        {/* Página para crear un nuevo merchant */}
+        <Route path="/merchants-create" element={<MerchantFormPage />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
