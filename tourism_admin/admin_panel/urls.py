a0eb9efd8f68082e0228c685a345050a1f4107c8
Hollman Rojas <hollmanerojas@gmail.com>
2024-12-20 16:35:30 -0500

    Feature/ add-jwt-simple- configurations

--- tourism_admin/admin_panel/urls.py
+++ tourism_admin/admin_panel/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers #tomar la vista genera todas la urls
-from .views import MerchantView
-from rest_framework.documentation import include_docs_urls
+from .views import MerchantView, RegisterUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = routers.DefaultRouter()
 #router.register(r'user', views.User, 'users')
@@ -10,4 +10,7 @@ router.register(r'merchants', MerchantView, basename='merchant')
 urlpatterns = [
     path('api/v1/', include(router.urls)),
     #path('docs/', include_docs_urls(title=('Merchants Api')))
+    path('api/v1/register/', RegisterUserView.as_view(), name='register'),
+    path('api/v1/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- tourism_admin/admin_panel/views.py
+++ tourism_admin/admin_panel/views.py
@@ -1,11 +1,34 @@
-from .models import Merchant
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Merchant, User
+from django.contrib.auth.hashers import make_password
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from .serializers import  MerchantSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 
 class MerchantView(ModelViewSet):
      queryset =Merchant.objects.all()
      serializer_class = MerchantSerializer
-     #querysetUser = User.objects.all()
+     permission_classes = [IsAuthenticated]
      
+class RegisterUserView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            data = request.data
+            if User.objects.filter(username=data['username']).exists():
+                return Response({"error": "Username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+            user = User.objects.create(
+                username=data['username'],
+                email=data['email'],
+                password=make_password(data['password']),
+            )
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
