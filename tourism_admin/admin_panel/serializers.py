a43861543df4ad2dba6582f492316a17b1bb40f3
Hollman Rojas <hollmanerojas@gmail.com>
2024-12-28 04:46:05 -0500

    Feature/ Api-login-and-register-jwt

--- tourism_admin/admin_panel/serializers.py
+++ tourism_admin/admin_panel/serializers.py
@@ -7,6 +7,13 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+    def create(self, validated_data):
+        user = User.objects.create_user(
+           **validated_data)
+        return user
 class MerchantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Merchant
--- tourism_admin/admin_panel/tests.py
+++ tourism_admin/admin_panel/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse, resolve
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import MerchantView, RegisterUserView
+
+# FILE: tourism_admin/admin_panel/test_urls.py
+
+
+class TestUrls(APITestCase):
+
+    def test_register_url(self):
+        url = reverse('register')
+        self.assertEqual(resolve(url).func.view_class, RegisterUserView)
+
+    def test_token_obtain_pair_url(self):
+        url = reverse('token_obtain_pair')
+        self.assertEqual(resolve(url).func.view_class, TokenObtainPairView)
+
+    def test_token_refresh_url(self):
+        url = reverse('token_refresh')
+        self.assertEqual(resolve(url).func.view_class, TokenRefreshView)
+
+    def test_merchants_url(self):
+        url = reverse('merchant-list')
+        self.assertEqual(resolve(url).func.view_class, MerchantView)
